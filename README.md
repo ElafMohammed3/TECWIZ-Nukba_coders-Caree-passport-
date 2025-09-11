403e2c83d2acef190a599a8824c3fe897fa16f8e
ElafMohammed3 <elafmohammedalhalili@gmail.com>
2025-09-11 14:12:10 +0300

    more

---
+++ README.md
@@ -0,0 +1,2 @@
+# TECWIZ-Nukba_coders-Caree-passport-
+The project is a 'Next Step' initiative and a platform designed to support graduates, students, and professionals.
---
+++ src/pages/feedback.js
@@ -0,0 +1,21 @@
+function feedback() {
+  return (
+    <main class="feedback-container">
+    <div id="initial-state">
+        <h1>Your Feedback Matters</h1>
+        <p>How was your experience today?</p>
+       
+        <div class="emoji-selection">
+        </div>
+    </div>
+
+    <form id="feedback-form">
+    </form>
+   
+    <div id="success-message" class="success-message">
+    </div>
+</main>
+  );
+}
+
+export default feedback;
